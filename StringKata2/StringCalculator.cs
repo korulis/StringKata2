c7a7d21e602180b1fc82dd13ff096f6cb2195d9f
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2017-01-13 09:37:07 +0200

    Zero test and test template.

---
+++ StringKata2/StringCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace StringKata2
+{
+    public class StringCalculator
+    {
+        public int Add(string empty)
+        {
+            return 0;
+        }
+    }
+}
--- StringKata2Tests/StringCalculatorTests.cs
+++ StringKata2Tests/StringCalculatorTests.cs
@@ -1,20 +1,33 @@
 ﻿using NUnit.Framework;
+using StringKata2;
 
 namespace StringKata2Tests
 {
     [TestFixture]
     public class StringCalculatorTests
     {
+        private StringCalculator _sut;
+
         [SetUp]
         public void Init()
         {
-            
+            _sut = new StringCalculator();
         }
 
         [Test]
-        public void Test()
+        public void Zero()
         {
-            
+            var actual = _sut.Add(string.Empty);
+
+            Assert.AreEqual(0, actual);
+        }
+
+
+
+        [Test]
+        public void TemplateTest()
+        {
+            var actual = _sut.Add(string.Empty);
         }
     }
 }
