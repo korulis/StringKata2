f7390a3a23fdc63e99833908fb1da647e40ad402
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2017-01-13 09:04:22 +0200

    Init with git ignore.

---
+++ StringKata2Tests/StringCalculatorTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace StringKata2Tests
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [SetUp]
+        public void Init()
+        {
+            
+        }
+
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
