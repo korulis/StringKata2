d9e5fbf09bdd77c7e2b67daa7b606008669a6441
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2017-01-18 09:39:14 +0200

    Zero, One and Nothing in one test.

---
+++ StringKata2/StringCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace StringKata2
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            if (numbers == "") return 0;
+
+            var parsedNumber = int.Parse(numbers);
+
+            return parsedNumber;
+        }
+    }
+}
--- StringKata2Tests/StringCalculatorTests.cs
+++ StringKata2Tests/StringCalculatorTests.cs
@@ -1,20 +1,35 @@
 ﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using Ploeh.AutoFixture.NUnit3;
+using StringKata2;
 
 namespace StringKata2Tests
 {
     [TestFixture]
     public class StringCalculatorTests
     {
+        private StringCalculator _sut;
+
         [SetUp]
         public void Init()
         {
-            
+            _sut = new StringCalculator();
         }
 
+
+
         [Test]
-        public void Test()
+        [InlineAutoData(0, "0")]
+        [InlineAutoData(0, "")]
+        [InlineAutoData(1, "1")]
+        public void Adds(int expectedSum, string numbers)
         {
-            
+            var actual = _sut.Add(numbers);
+
+            Assert.AreEqual(expectedSum, actual);
         }
+
+
     }
+
 }
