860a6dbfa1cbe374363dc1fa076dd144d25ecd05
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2017-01-19 11:38:29 +0200

    up to adding with line breaks.

---
+++ StringKata2/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace StringKata2
+{
+    public class StringCalculator
+    {
+        public int Add(string input)
+        {
+            if (input == string.Empty) return 0;
+
+            var sum = input.Split(new[] { ",", "\n" }, StringSplitOptions.None)
+                .Where(s => s != string.Empty)
+                .Select(int.Parse)
+                .Aggregate(Sum);
+            return sum;
+
+        }
+
+        private static int Sum(int s1, int s2)
+        {
+            return s1 + s2;
+        }
+    }
+}
--- StringKata2Tests/StringCalculatorTests.cs
+++ StringKata2Tests/StringCalculatorTests.cs
@@ -1,20 +1,61 @@
 ﻿using NUnit.Framework;
+using Ploeh.AutoFixture.NUnit3;
+using StringKata2;
 
 namespace StringKata2Tests
 {
     [TestFixture]
     public class StringCalculatorTests
     {
+        private StringCalculator _sut;
+
         [SetUp]
         public void Init()
         {
-            
+            _sut = new StringCalculator();
         }
 
         [Test]
-        public void Test()
+        [InlineAutoData(0, "")]
+        [InlineAutoData(1, "1")]
+        [InlineAutoData(2, "2")]
+        public void AddSingle(int expected, string input)
         {
-            
+            var actual = _sut.Add(input);
+
+            Assert.AreEqual(expected, actual);
         }
+
+        [Test]
+        [InlineAutoData(2, "1,1")]
+        [InlineAutoData(3, "2,1")]
+        [InlineAutoData(2, "2,")]
+        [InlineAutoData(4, "2,1,1")]
+        public void AddMultiple(int expected, string input)
+        {
+            var actual = _sut.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [InlineAutoData(6, "1\n2,3")]
+        [InlineAutoData(1, "1,\n")]
+        public void AddWithLineBreaks(int expected, string input)
+        {
+            var actual = _sut.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [InlineAutoData(3, "//;\n1;2")]
+        public void ChangeDelimiter(int expected, string input)
+        {
+            var actual = _sut.Add(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
     }
 }
